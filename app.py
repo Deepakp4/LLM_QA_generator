d27db5d69ca900f2b76e1161a9c3b448f4bb5f11
Deepak Prajapati <86311462+Deepakp4@users.noreply.github.com>
2024-01-30 19:00:36 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+import os
+from dotenv import load_dotenv
+from langchain.llms import OpenAI
+from langchain.agents import AgentType, initialize_agent, load_tools
+from langchain_community.callbacks import StreamlitCallbackHandler
+import streamlit as st
+
+load_dotenv()
+
+llm = OpenAI(temperature=0, streaming=True, openai_api_key=os.getenv("OPENAI_API_KEY"))
+tools = load_tools(["ddg-search"])
+agent = initialize_agent(
+    tools,
+    llm,
+    agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+    # verbose=True
+)
+
+
+if prompt := st.chat_input():
+    st.chat_message("user").write(prompt)
+    with st.chat_message("assistant"):
+        st.write("...")
+        st_callback = StreamlitCallbackHandler(st.container())
+        response = agent.run(prompt, callbacks=[st_callback])
+        st.write(response)
