f47d4147aa4b480011a280698d67d0769a3c02c3
Deepak Prajapati <86311462+Deepakp4@users.noreply.github.com>
2024-01-30 19:11:04 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ from langchain.llms import OpenAI
 from langchain.agents import AgentType, initialize_agent, load_tools
 from langchain_community.callbacks import StreamlitCallbackHandler
 import streamlit as st
+# Loading .env file
 
 load_dotenv()
 
+# Accessing Openai api key and initializing agent
 llm = OpenAI(temperature=0, streaming=True, openai_api_key=os.getenv("OPENAI_API_KEY"))
 tools = load_tools(["ddg-search"])
 agent = initialize_agent(
@@ -16,7 +18,7 @@ agent = initialize_agent(
     # verbose=True
 )
 
-
+# creating prompt template
 if prompt := st.chat_input():
     st.chat_message("user").write(prompt)
     with st.chat_message("assistant"):
